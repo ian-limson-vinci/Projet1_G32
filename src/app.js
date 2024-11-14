fd13adacdad3b716b33d0f74fcc574d9d45e29eb
Ian Limson <ian.limson@student.vinci.be>
2024-11-14 16:33:22 +0100

    Refactor fiveMinutesBlocks

--- src/app.js
+++ src/app.js
@@ -7,9 +7,11 @@ class Main {
     fiveMinutesBlocks(minutes) {
         const lamps = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']
         const lampsOn = Math.floor(minutes/5);
+        
         for (let i=0; i < lampsOn; i++) {
             lamps[i] = (i+1) % 3 === 0 ? 'R' : 'Y';
         }
+
         return lamps.join(' ');
     }
 }
