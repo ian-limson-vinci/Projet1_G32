15d008910ac7245641d87366369e89cc2ddd6d7e
uche okele <uche.okele@student.vinci.be>
2024-11-14 17:36:23 +0100

    test etape 3

--- src/spec/appSpec.js
+++ src/spec/appSpec.js
@@ -8,4 +8,20 @@ describe("Berlin Clock tests", function() {
         
         expect(result).toBe("Y Y Y O")
     })
+
+
+
+
+
+
+
+
+    
+    it('should turn on the correct number of lamps for hours', () => {
+      expect(simpleHeure(0)).toBe('0000');
+      expect(simpleHeure(1)).toBe('1000');
+      expect(simpleHeure(2)).toBe('1100');
+      expect(simpleHeure(3)).toBe('1110');
+      expect(simpleHeure(4)).toBe('1111');
+    });
 }); 
