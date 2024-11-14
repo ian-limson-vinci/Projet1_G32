ace2a286fe9aa9fc447993f2d0691f367f03629d
Ian Limson <ian.limson@student.vinci.be>
2024-11-14 16:31:54 +0100

    Implementation fiveMinutesBlocks

--- src/app.js
+++ src/app.js
@@ -3,6 +3,19 @@ class Main {
         const lampsOn = minutes % 4;
         return ['O','O','O','O'].map((lamp, index) => (index < lampsOn ? 'Y' : lamp)).join(' ');
     }
+
+    fiveMinutesBlocks(minutes) {
+        const lamps = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']
+        const lampsOn = Math.floor(minutes/5);
+        for (let i=0; i < lampsOn; i++) {
+            if ((i+1) % 3 === 0 && i !== 0) {
+                lamps[i] = 'R';
+            } else {
+                lamps[i] = 'Y';
+            }
+        }
+        return lamps.join(' ');
+    }
 }
 
 module.exports = { Main };
