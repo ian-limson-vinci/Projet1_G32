fcd3d84cba784417d33e8114cf6c8b85b659334f
Ian Limson <ian.limson@student.vinci.be>
2024-11-14 16:24:33 +0100

    Test fiveMinutesBlocks

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -6,6 +6,12 @@ describe("Berlin Clock tests", function() {
     it("Should return correct simple minutes representation", function() {
         let result = main.simpleMinutes(3);
         
-        expect(result).toBe("Y Y Y O")
+        expect(result).toBe("Y Y Y O");
+    })
+
+    it("Should return correct five minutes blocsk representation", function() {
+        let result = main.fiveMinutesBlocks(23);
+
+        expect(result).toBe("Y Y R Y O O O O O O O");
     })
 }); 
