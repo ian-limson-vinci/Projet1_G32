1c2bafe695befd2d1f9d6d90fafd7ae3a698d7e7
uche okele <uche.okele@student.vinci.be>
2024-11-14 19:38:43 +0100

    test reussi etape 4

--- src/app.js
+++ src/app.js
@@ -21,8 +21,22 @@ heureSimple(hours){
     return '1'.repeat(hours) + '0'.repeat(4 - hours);
 }
 
+cinqHeure(hours){
+    let ampoules2 = '0000';
+
+    const allumees = Math.floor(hours / 5);
+
+    for (let i = 0; i < allumees; i++) {
+        ampoules2 = ampoules2.substring(0, i) + '1' + ampoules2.substring(i + 1);
+    }
+
+    return ampoules2;
+
 
 
 }
+}
+
+module.exports = { cinqHeure };
 module.exports = { heureSimple };
 module.exports ={Main};
